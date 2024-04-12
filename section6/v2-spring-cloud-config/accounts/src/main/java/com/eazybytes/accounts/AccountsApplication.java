f1839cbd7cd976d89f6912b43cc57bff21e17130
Raghunath D <rndayala@gmail.com>
2024-04-12 17:09:52 +0530

    Configuration related changes are added

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,47 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+// this is Accounts Application main Spring Boot file
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Raghunath D",
+						email = "mail@rndayala.com",
+						url = "https://www.rndayala.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.rndayala.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "Accounts microservice REST API Documentation",
+				url = "https://www.rndayala.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+}
---
+++ section7/accounts/src/main/java/com/eazybytes/accounts/entities/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.eazybytes.accounts.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@ToString
+public class BaseEntity {
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt; // maps to column : `created_at` date NOT NULL
+
+    @Column(updatable = false)
+    @CreatedBy
+    private String createdBy;        // maps to column : `created_by` varchar(20) NOT NULL
+
+    @Column(insertable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt; // maps to column : `updated_at` date DEFAULT NULL
+
+    @Column(insertable = false)
+    @LastModifiedBy
+    private String updatedBy;        // maps to column : `updated_by` varchar(20) DEFAULT NULL
+}
