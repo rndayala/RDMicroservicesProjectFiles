2b999ef87c741eb58bf7351d0a849c941cbe2853
Raghunath D <rndayala@gmail.com>
2024-09-01 14:37:05 +0530

    section 11 - observability monitoring metrics in microservices

---
+++ section11/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,26 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+// this is the entity class that represents accounts table in the database
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity {
+    @Column(name="customer_id")
+    private Long customerId;      // maps to column name : `customer_id`
+
+    @Column(name="account_number")
+    @Id
+    private Long accountNumber;   // maps to column name : `account_number`
+
+    @Column(name="account_type")
+    private String accountType;   // maps to column name : `account_type`
+
+    @Column(name="branch_address")
+    private String branchAddress; // maps to column name : `branch_address`
+}
